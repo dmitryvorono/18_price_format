33c934e2709b60d560564db0db765158dc176c40
dvoronov <voronov-d88@yandex.ru>
2017-07-11 08:09:59 +0300

    Fix bug - fogot import format_price

--- tests.py
+++ tests.py
@@ -1,29 +1,31 @@
 # TODO place your tests here
 import unittest
+import format_price
+
 
 class TestFormatPrice(unittest.TestCase):
     def test_empty_string(self):
-        self.assertEqual(format_price(''), '')
+        self.assertEqual(format_price.format_price(''), '')
 
 
     def test_none(self):
-        self.assertEqual(format_price(None), '')
+        self.assertEqual(format_price.format_price(None), '')
 
 
     def test_zero_fractional_part(self):
-        self.assertEqual(format_price('245.000000'), '245')
+        self.assertEqual(format_price.format_price('245.000000'), '245')
     
 
     def test_round_zero_fractional_part(self):
-        self.assertEqual(format_price('245.001000'), '245')
+        self.assertEqual(format_price.format_price('245.001000'), '245')
 
 
     def test_numeric_digit(self):
-        self.assertEqual(format_price('123456789'), '123 456 789')
+        self.assertEqual(format_price.format_price('123456789'), '123 456 789')
 
 
     def test_usial_string(self):
-        self.assertEqual(format_price('3245.010001', '3 245.01'))
+        self.assertEqual(format_price.format_price('3245.010001', '3 245.01'))
 
     
 if __name__ == '__init__':
