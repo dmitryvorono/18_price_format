eddbdc5c25808ebaa96f8f44723296cc303a2fe7
dvoronov <voronov-d88@yandex.ru>
2017-07-13 08:19:32 +0300

    Add format_fractional_part

--- format_price.py
+++ format_price.py
@@ -1,5 +1,22 @@
 def format_price(price):
-    pass
+    if not price:
+        return ''
+    try:
+        float_price = float(price)
+    except ValueError:
+        return ''
+    integer_part = round(float_price)
+    fractional_part = float_price - integer_part
+    return ''.join([str(integer_part), format_fractional_part(fractional_part)])
+
+
+def format_fractional_part(fractional_part):
+    round_coins = 2
+    rounded_fractional_part = round(fractional_part, round_coins)
+    if not rounded_fractional_part:
+        return ''
+    return str(rounded_fractional_part)[1:]
+
 
 if __name__ == '__main__':
-    pass
+    print(format_price('3245.000000'))
